b0aa7bf4044d3a6cad8ede792fbf208ded394e4c
codewthjava <abhisunnii@gmail.com>
2023-08-30 10:19:44 +0530

    todo-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,23 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Todo App';
+
+  course: string = '';
+  courses: string[] = [];
+  placeHolderText: string = 'Enter Course';
+
+  constructor() {}
+
+  addCourse() {
+    this.courses.push(this.course.toUpperCase());
+    this.course = '';
+  }
+
+  removeCourse(index: number) {
+    this.courses.splice(index, 1);
+  }
 }
